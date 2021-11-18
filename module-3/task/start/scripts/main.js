db7ea808cc17c5355d33f9bd5bc44a1e323f8430
Alex Thomas <92339139+AJT8112@users.noreply.github.com>
2021-11-18 21:04:14 +0000

    Addition of drinks vender and calculator

--- module-3/task/start/scripts/main.js
+++ module-3/task/start/scripts/main.js
@@ -58,3 +58,80 @@ function productDiscount(){
     console.log(sum);
 }
 productDiscount();
+
+//FUNCTION called drinkOrder has 2 perameters Drink and Size
+function drinkOrder(size,drink){
+    //VALIDATE the variables passed through.
+    if(size != 'small' && size != 'medium' && size != 'large'){
+        console.log('You have ordered a drink we don\'t sell');
+        return;
+    }
+    let message = 'You have ordered a ' + size;
+    //SWITCH drink
+    switch(drink){
+        //CREATE a case for each drink
+        case 'cola':
+            message +=  ' Coca-Cola';
+            break;
+        case 'lemon':
+            message +=  ' Schwepps';
+            break;
+        case 'orange':
+            message +=  ' Tango';
+            break;
+        default: 
+            message = 'You have ordered a drink we don\'t sell'
+            break;
+    }
+   console.log(message);
+}
+drinkOrder('large','cola');
+drinkOrder('humungous','milkshake')
+
+function calculator(num1, num2, operator){
+    if(isNaNValidation(num1)){
+        console.log('Number 1 is not a number')
+        return;
+    }
+    if(isNaNValidation(num2)){
+        console.log('Number 2 is not a number')
+        return;
+    }
+    let sum;
+    switch(operator){
+        case '+':
+            sum = num1 + num2;
+            break;
+        case '-':
+            sum = num1 - num2;
+            break;
+        case '/':
+            sum = num1 / num2;
+            break;    
+        case '*':
+            sum = num1 * num2;
+            break;  
+        case '%':
+            sum = num1 % num2;
+            break;
+        default:
+            console.log('Operator is not recognised');
+            return;
+    }
+    return sum;
+}
+
+function isNaNValidation(number){
+    return isNaN(number);
+}
+
+let sum1 = calculator(10,2,'+');
+let sum2 = calculator(10,2,'-');
+let sum3 = calculator(10,2,'/');
+let sum4 = calculator(10,2,'*');
+let sum5 = calculator(10,2,'%');
+console.log(sum1);
+console.log(sum2);
+console.log(sum3);
+console.log(sum4);
+console.log(sum5);
